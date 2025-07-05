fccc20decc00ad2fdfd757616bde0009cdc6e603
nataliyarosenberg <nshcherbak@yahoo.com>
2025-07-05 11:12:31 -0700

    HW markup

---
+++ src/js/2-form.js
@@ -0,0 +1,62 @@
+const formData = {
+  email: '',
+  message: '',
+};
+const { email, message } = formData;
+
+const form = document.querySelector('form');
+// form.addEventListener('submit', handleSubmit);
+// form.addEventListener('input', handleInput);
+
+const feedback_key = 'feedback-form-state';
+
+function saveFormData() {
+  localStorage.setItem(feedback_key, JSON.stringify(formData));
+}
+
+function populateForm() {
+  try {
+    const savedData = localStorage.getItem(feedback_key);
+    if (savedData) {
+      const parseData = JSON.parse(savedData);
+      Object.assign(formData, parseData);
+      if (
+        form.elements.email.value === '' ||
+        form.elements.message.value === ''
+      ) {
+        return 'Please fill all fields';
+      }
+      form.elements.email.value = formData.email;
+      form.elements.message.value = formData.message;
+    }
+  } catch (error) {
+    console.error('Error parsing', error);
+    localStorage.removeItem(feedback_key);
+  }
+}
+
+function handleInput(event) {
+  formData[event.target.name] = event.target.value;
+  saveFormData();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  if (formData.email.trim() === '' || formData.message.trim() === '') {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log('Form submitted:', formData);
+
+  localStorage.removeItem(feedback_key);
+
+  event.currentTarget.reset();
+
+  formData.email = '';
+  formData.message = '';
+}
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('input', handleInput);
+
+populateForm();
